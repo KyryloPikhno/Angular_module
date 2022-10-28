4eeefd8e9942bf00625f369fa0cd82953dbbb6c2
KyryloPikhno <Mr.Good@i.ua>
2022-10-28 17:09:02 +0300

    initial commit

---
+++ src/app/components/comment/comment.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+import {IComment} from  '../../Interfaces'
+
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+  @Input()
+  comment:IComment
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import {PostService} from "../../services";
+import {IPost} from "../../Interfaces";
+
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts: IPost[];
+
+  constructor(private postService: PostService) {
+
+  }
+
+  ngOnInit(): void {
+    this.postService.getAll().subscribe(value => this.posts = value)
+  }
+
+  getAllPosts(): void{
+  }
+
+}
